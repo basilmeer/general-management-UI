6e3ac0b50d7da98079e10d8c0841fb3294f0b068
basilmeer <basilmeer@gmail.com>
2017-03-13 21:36:41 +0500

    Tree view works on index.html

--- js/script.js
+++ js/script.js
@@ -1,12 +1,33 @@
 $(document).ready(function() {
   $('#view-inventory').hide();
   $('#add-inventory').hide();
+  $('#view-booking').hide();
+  $('#add-booking').hide();
+  $('#view-invoices').hide();
+  $('#add-invoices').hide();
+  $('#view-invoices').hide();
+  $('#add-invoices').hide();
+  $('#view-products').hide();
+  $('#add-products').hide();
+
   $('#inventory').click(function(){
     $('#add-inventory').slideToggle('slow');
     $('#view-inventory').slideToggle('slow');
   });
-  // $('#booking').click(function() {
-  //   $('add-booking').slideToggle('slow');
-  //   $('view-booking').slideToggle('slow');
-  // })
+
+  $('#booking').click(function() {
+    $('#add-booking').slideToggle('slow');
+    $('#view-booking').slideToggle('slow');
+  });
+
+  $('#invoices').click(function() {
+    $('#add-invoices').slideToggle('slow');
+    $('#view-invoices').slideToggle('slow');
+  });
+
+
+  $('#products').click(function() {
+    $('#add-products').slideToggle('slow');
+    $('#view-products').slideToggle('slow');
+  });
 });
